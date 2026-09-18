7ed5dbd4374228c6e42eacc437da1304a797a9d9
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2026-09-18 22:49:39 +0300

    Register Question model to admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from polls.models import Question
+
+
+admin.site.site_header = "Polls Admin"
+admin.site.site_title = "The world's coolest Admin Panel"
+admin.site.index_title = "Polls Site"
 
 # Register your models here.
+admin.site.register(Question)
